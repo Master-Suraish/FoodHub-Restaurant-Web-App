ed06032dc9887a7568814a74cfc3927923a7bc99
Suraish Kirshan <sureshkamar3000@gmail.com>
2026-03-12 02:21:10 +0500

    Removes accidentally added extra commas in auth.controllers.ts

--- Backend/src/features/auth/auth.controller.ts
+++ Backend/src/features/auth/auth.controller.ts
@@ -218,18 +218,17 @@ export const loginAdmin = async (req: Request, res: Response) => {
 
     const isProduction = process.env.NODE_ENV === "production";
 
-
     res.cookie("token", accessToken, {
       httpOnly: true,
       secure: isProduction,
-      sameSite:  isProduction ? "none" : "lax",,
+      sameSite: isProduction ? "none" : "lax",
       maxAge: 60 * 60 * 1000,
     });
 
     res.cookie("refreshToken", refreshToken, {
       httpOnly: true,
       secure: isProduction,
-      sameSite:  isProduction ? "none" : "lax",,
+      sameSite: isProduction ? "none" : "lax",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
