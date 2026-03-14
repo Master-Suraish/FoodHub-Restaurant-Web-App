5f90561eaff48ed64930a742d6ce08f287bb76de
Suraish Kirshan <sureshkamar3000@gmail.com>
2026-03-14 22:30:25 +0500

    Add service, change port and secure true to fixing email send in production

--- Backend/src/utils/sendEmail.ts
+++ Backend/src/utils/sendEmail.ts
@@ -6,9 +6,10 @@ import {
 
 const createTransporter = () => {
   return nodemailer.createTransport({
+    service:"Gmail",
     host: "smtp.gmail.com",
-    port: 587,
-    secure: false,
+    port: 465,
+    secure: true,
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
